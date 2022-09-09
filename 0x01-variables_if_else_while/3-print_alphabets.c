b7524d879cb849a0a8e6ebd6ec47c9bbef566975
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-09 23:16:33 +0000

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,29 +10,18 @@
 
 int main(void)
 {
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
+	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-
-			putchar(low);
-
-	}
-
-
-	for (low = 'A'; low <= 'Z'; low++)
-
-	{
 
 		putchar(low);
-
-	}
+	
+	for (low = 'A'; low <= 'Z'; low++)
+		
+		putchar(low);
+	
 	putchar('\n');
 
+	
 	return (0);
-
 }
