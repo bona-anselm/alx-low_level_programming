86b473bb8f9ae964ae2bfdc71abad8bbf5f211cb
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 18:41:51 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
-
+/**
+ * print_alphabet_x10 - prints alphabets 10x
+ *
+ * Description: prints alphabets 10x in lower case
+ * Return: Nothing
+ */
 void print_alphabet_x10(void)
 {
 	char k;
 	char i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (k = 'a'; k <= 'z'; k++)
 			_putchar(k);
