8b6ebedf52574427c59dce093fe16722be42925c
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 13:47:45 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
 /**
- * * main - prints _putchar,followed by a new line
- * * Return: Always 0 (Success)
- * */
+ * main - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char text[10] = "_putchar";
+	
+	char word[8] = "_putchar";
 	int i = 0;
+
 	for (i = 0; i < 8; i++)
 
 	{
-		_putchar(text[i]);
+
+		_putchar(word[i]);
+
 	}
+
 	_putchar('\n');
+
 	return (0);
 
 }
