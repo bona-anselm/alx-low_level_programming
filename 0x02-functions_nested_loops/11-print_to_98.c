e5661b34b41bdf1dda408d0a2abbc80e33b081de
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 22:33:14 +0000

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,22 +11,18 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)			
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
 
 			if (n == 98)
 
 				continue;
-			
 			printf(", ");
 			
 		}
-		
 		printf("\n");
-
 	}
-
 	else
 	{
 		for (; n >= 98; n--)
@@ -38,12 +34,9 @@ void print_to_98(int n)
 				continue;
 			
 			printf(", ");
-
 		}
 		
 		printf("\n");
-
 	}
-
 }
 
