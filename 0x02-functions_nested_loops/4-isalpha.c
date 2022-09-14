7b2b38c43086842cfedda2fd7d94511eb0543385
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 19:36:12 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * isalpha - checks for alphabets
+ *@c: Holds input character
+ *
+ * Description: Checks if a character is lower or upper case
+ * Return: 1 if is lower and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+
+	int isalpha = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+
+		if (c == lower || c == upper)
+
+			isalpha = 1;
+		}
+	}
+
+	return (isalpha);
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,4 +4,5 @@
 |**1-alphabet.c**          |This function prints the alphabet in lowercase		|
 |**2-print_alphabet_x10.c**|This function prints 10 times the alphabet in lowercase	|
 |**3-islower**		   |This function checks for lowercase character		|
+|**4-isalpha.c**	   |This function checks for alphabetic character		|
 |
