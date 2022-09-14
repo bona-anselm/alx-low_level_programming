c6acff6c6dc377cdb6db6292b9c46009f1abb31c
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 20:08:59 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,24 @@
 int print_sign(int n)
 {
 	int val;
+
 	if (n > 0)
 	{
+
 		val = 1;
 		_putchar('+');
 	}
+
 	else if (n == 0)
 	{
+
 		val = 0;
 		_putchar('0');
 	}
+
 	else
 	{
+
 		val = -1;
 		_putchar('-');
 	}
