36323a568019624183f0f6fbbfe6243a27bb9d62
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 12:52:46 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,6 @@
 #include <unistd.h>
 
 
-/**
- * main - Entry point
- * Description: It prints the word Holberton, followed by a new line.
- *
- * Return: 0.
- */
-
-int _putchar(char c);
-
-int main(void)
-{
-	char word[8] = "_putchar";
-	
-	int i;
-	
-	for (i = 0; i < 8; i++)
-
-		_putchar(word[i]);
-
-	_putchar('\n');
-
-	return (0);
-}
-
 
 /**
  * _putchar - writes the character c to stdout
