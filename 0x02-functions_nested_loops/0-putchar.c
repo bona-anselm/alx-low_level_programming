1e9bbdb0d7e64713f1f9c02efdf6b518fd7a38f6
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-14 13:51:24 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	
-	char word[8] = "_putchar"
-	int i = 0
+	char word[8] = "_putchar";
+	int i = 0;
 
 	for (i = 0; i < 8; i++
 
 	{
 
-		_putchar(word[i])
+		_putchar(word[i]);
 
 	}
 
-	_putchar('\n')
+	_putchar('\n');
 
 	return (0);
 
