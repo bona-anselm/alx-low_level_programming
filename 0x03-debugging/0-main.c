8a45dae67ac2a2e7f9f5e22245f119ea5d00990e
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-15 07:02:05 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if an integer is -ve or +ve
+ * Description: Paricular tests whether 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+|**FUNCTION**		|**DESCRIPTION**						|
+|-----------------------|---------------------------------------------------------------|
+|**0-main**		|This program tests whether an integer is positive or negative	|
+
