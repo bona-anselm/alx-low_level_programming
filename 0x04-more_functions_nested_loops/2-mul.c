bc631ebb1f16b1b223fb0145ea2d76098ec67557
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-17 03:10:02 +0000

    Collaboration is multipication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: Holds the input data for first integer
+ * @b: Holds input data for second integer
+ *
+ * Description: Multiplies two integers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+	return (0);
+}
