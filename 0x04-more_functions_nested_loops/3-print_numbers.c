7faccd8af8dee4764b46c33dd26b4ac692043fce
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-17 03:34:11 +0000

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - prints numbers 0 - 9
- *
  * Return: 0 always
  */
 void print_numbers(void)
@@ -10,6 +9,7 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
+
 		_putchar(i);
 
 	_putchar('\n');
