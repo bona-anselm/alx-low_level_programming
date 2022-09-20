586168c4b91c4719a75a4ec1c9d8839c21dcba6c
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-20 11:08:43 +0000

    Prints in half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: Holds input data
+ *
+ * Description: Prints half of a string followed by a new line
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i, len, n;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
