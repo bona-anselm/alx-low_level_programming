79d131148ba47e1190add427c583a51f1fb22389
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-20 12:17:00 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Copies a string
+ * @src: Pointer the string source
+ * @dest: Pointer to the string destination
+ *
+ * Description: Copies a string from source to destination
+ * Return: destination pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *str;
+
+	str = dest;
+
+	while (*src)
+
+		*dest++ = *src++;
+
+	return (str);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -9,4 +9,5 @@
 |**6-void puts2.c**		|This prints every other character of a string, starting from the first |
 |**7-puts_half.c**		|This function prints half of a string.					|
 |**8-print_array.c**		|This prints ```n``` elements an array of integers			|
-
+|**9-strcpy.c**			|This function copies a string from one source to a destination		|
+|
