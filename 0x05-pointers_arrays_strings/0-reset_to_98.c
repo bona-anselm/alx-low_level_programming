bed191db92cffa463ea410c6a88d8265af692c01
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-20 07:00:50 +0000

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of a pointer
+ * @n: Holds the data
+ *
+ * Description: uses a pointer to reset a variable value to 98
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{	
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+|FUNCTIONS			|DESCRIPTIONS								|
+|-------------------------------|--------------------------------------------------------------- -------|
+|**0-reset_to_98.c**		|Uses pointer to update the value of the parameter it points to		|
+
