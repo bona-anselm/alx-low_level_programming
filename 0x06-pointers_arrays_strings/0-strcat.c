98041be497ea53886599b53f6fa4232805966b4c
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-21 12:03:08 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,26 +7,26 @@
  * @src: Holds input data for source strings
  *
  * Description: Concatenate source and destinations strings
- * Return: 0
+ * Return: A pointer to the  concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
 
-	int i, j;
+	char *temp;
 
-	i = 0;
+	temp = dest;
 
-	while (dest[i] != '\0')
+	while (*dest)
 	{
-		i++;
+		dest++;
 	}
 
-	for (j = 0; src[j] != '\0'; j++, i++)
+	while (*src)
 	{
-		dest[i] = src[j];
+		*dest++ = *src++;
 	}
 
-	dest[i] = '\0';
+	*dest = '\0';
 
-	return (0);
+	return (temp);
 }
