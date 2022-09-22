123c7fdc7cbc2bff7090ad80669732709271c485
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-22 18:21:57 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - Compares two string
+ * @s1: Holds input data for s1 strings
+ * @s2: Holds input data for s2 string
+ *
+ * Description: Concatenate source and destinations strings
+ * Return: The difference between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (*s1 - *s2);
+		}
+
+		s1++, s2++;
+
+	}
+
+	return (*s1 - *s2);
+
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,4 +3,5 @@
 |**0-strcat.c**		|This function concatenates two strings				|
 |**1-strncat.c**	|This function concatenates two strings				|
 |**2-strncpy.c**	|This function copies string from source to destination		|
+|**3-strcmp.c**		|This function compares two strings				|
 
