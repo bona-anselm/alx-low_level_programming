be4850a365db04e3d6f9f4de4e698e758ea99dff
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-22 18:32:46 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 && s2)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 
 			return (*s1 - *s2);
 
