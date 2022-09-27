fedb0af2b984e77345b7229b41606d6207b97153
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-09-27 06:25:46 +0000

    Double Pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: Holds the data of pointer to change
+ * @to: Holds the value to change the pointer to
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
