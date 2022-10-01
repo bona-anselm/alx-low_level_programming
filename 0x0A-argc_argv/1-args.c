5cae34936ca9b3190b22e1d49f52970ba94fa572
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-10-01 03:13:48 +0000

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <main.h>
 #include <stdio.h>
 
 /**
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	/*(void) argv;*/
 
 	printf("%d\n", argc);
 
