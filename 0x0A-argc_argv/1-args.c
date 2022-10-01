b3faacecdd58accc9b2ddae50d541f7130fb9558
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-10-01 03:08:23 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - A function that prints its number of aeguments
+ * @argc: Holds argument count
+ * @argv: Holds an array of strings of all argument passed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
