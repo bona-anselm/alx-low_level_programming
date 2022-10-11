38954a04e4d350c1680710ed892efa63049ebedb
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-10-11 11:41:46 +0000

    corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,14 @@
  *
  * Description: It contains differrent element of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
