402826d9092de5fb97662492b2b739bfb08c2aba
Bonaventure Anselm <bona.anselm@ygmail.com>
2022-10-11 12:39:31 +0000

    new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - creates a new struct of type dog
+ * @name: Holds struct data for name
+ * @age: Holds struct data for age
+ * @owner: Holds struct data for owner
+ *
+ * Return: returns pointer to buffer of datatype dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name;
+	char *new_owner;
+	int len;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	len = 0;
+	while (name[len] != '\0')
+		len++;
+	len++;
+	new_name = malloc(len * sizeof(*owner));
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	len = 0;
+	while (name[len] != '\0')
+		len++;
+	len++;
+	new_owner = malloc(len * sizeof(*name));
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+	return (new_dog);
+}
