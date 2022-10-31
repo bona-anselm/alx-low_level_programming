a330fe7571239ea07afb05b69d8d6c269704f791
Bonaventure Anselm <bona-anselm@gmail.com>
2022-10-31 23:28:29 +0000

    read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buf[500];
+	ssize_t rd, wr; 
+	char *buf;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
-	read(fd, buf, letters);
-	buf[letters] = '\0';
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 	close(fd);
 
-	printf("%s\n", buf);
-	return (letters);
+	free(buf);
+	return (wr);
 }
