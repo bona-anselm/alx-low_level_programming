93e675d0eb2b3978e3c786b10a3788dd15e4124f
Bonaventure Anselm <bona.anselm@gmail.com>
2022-11-23 16:41:24 +0000

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,13 @@ int main(void)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-			if (i != j)
-			{
-				putchar(i);
-				putchar(j);
+			putchar(i);
+			putchar(j);
 
-				if (i == '8' && j == '9')
-					continue;
-
-				putchar(',');
-				putchar(' ');
-			}
+			if (i == '8' && j == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
