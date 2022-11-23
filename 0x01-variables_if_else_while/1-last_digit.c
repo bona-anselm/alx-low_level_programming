c9fd0b78a000e8be6a124dd11620bd3a9594c830
Bonaventure Anselm <bona.anselm@gmail.com>
2022-11-23 07:56:24 +0000

    Determines and describes the last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,33 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Determines & describes the last digit of a randomly generated number
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-	if (n % 10 > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if (n % 10 == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+
+	}
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	{
+
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
 
 	return (0);
 }
