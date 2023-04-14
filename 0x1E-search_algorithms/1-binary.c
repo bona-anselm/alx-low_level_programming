9def3a3a0a64574a7069fdc8913237e4ae16d5ae
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-14 12:47:13 +0100

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 
 		if (array[mid] == value)
 			return (mid);
-		else if (array[mid] > value)
+		else if (array[mid] < value)
 			right = mid - 1;
 		else
 			left = mid + 1;
