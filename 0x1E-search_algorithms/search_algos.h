80d807df1f295b33da16d01e109897818789c5b6
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-14 12:35:46 +0100

    header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_arr(int *arr, size_t left, size_t right);
 
 #endif
